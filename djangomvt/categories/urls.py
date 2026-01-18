cef487b927f0cdcb5ff8ca88a90d30a853e7e3ec
BloodyRoom <sabadashkamaksym@gmail.com>
2026-01-18 15:40:45 +0200

    api

--- djangomvt/categories/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name="categories"
-
-urlpatterns = [
-    path('', views.show_categories, name='show_categories'),
-    path('add/', views.add_category, name='add_category'),
-]
